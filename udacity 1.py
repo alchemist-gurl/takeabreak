69e5da9d10ad21d3a7517afa336db47e8ea5bbbd
Autumn Laniel <autumnlaniel@Autumns-MBP.lan>
2018-07-03 11:55:08 -0700

    changed the time amount

--- udacity 1.py
+++ udacity 1.py
@@ -3,8 +3,8 @@ import webbrowser
 
 total_breaks = 3
 break_count = 0
-print("started on " + time.ctime())
+print("time started at " + time.ctime())
 while(break_count < total_breaks):
-    time.sleep(2*60*60)
+    time.sleep(3*60*60)
     webbrowser.open("https://www.youtube.com/watch?v=itKtosLoGk4&frags=pl%2Cwn")
     break_count = break_count + 1
