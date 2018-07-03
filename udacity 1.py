e4b91371a8080fec9c2114fe3c37a578867b155e
Autumn Laniel <autumnlaniel@Autumns-MBP.lan>
2018-07-03 11:57:41 -0700

    add comments, time change

--- udacity 1.py
+++ udacity 1.py
@@ -1,10 +1,10 @@
 import time
 import webbrowser
 
-total_breaks = 3
+total_breaks = 3 #number of breaks in the day
 break_count = 0
 print("time started at " + time.ctime())
 while(break_count < total_breaks):
-    time.sleep(3*60*60)
+    time.sleep(2*60*60)
     webbrowser.open("https://www.youtube.com/watch?v=itKtosLoGk4&frags=pl%2Cwn")
     break_count = break_count + 1
